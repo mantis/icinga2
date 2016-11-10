46d71451d08d05ba572d7b3729730d50447a2da2
Michael Friedrich <michael.friedrich@icinga.com>
2016-11-10 20:00:00 +0100

    Ensure that runtime created objects are synced on (re)connect
    refs #11684

--- lib/remote/apilistener-configsync.cpp
+++ lib/remote/apilistener-configsync.cpp
@@ -412,10 +412,6 @@ void ApiListener::SendRuntimeConfigObjects(const JsonRpcConnection::Ptr& aclient
 			continue;
 
 		BOOST_FOREACH(const ConfigObject::Ptr& object, dtype->GetObjects()) {
-			/* don't sync objects with an older version time than the endpoint's log position */
-			if (object->GetVersion() < endpoint->GetLocalLogPosition())
-				continue;
-
 			/* don't sync objects for non-matching parent-child zones */
 			if (!azone->CanAccessObject(object))
 				continue;
